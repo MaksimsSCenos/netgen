eebc2619f3cc1b5ead216486709ee5905664ac6d
vgeza <vgeza@users.noreply.github.com>
2020-12-22 15:53:57 +0200

    Antennas v.1.0.0-beta5

--- libsrc/linalg/bfgs.cpp
+++ libsrc/linalg/bfgs.cpp
@@ -331,7 +331,6 @@ double BFGS (
 	{
 	  if (LDLtUpdate (l, d, 1 / a1, y) != 0)
 	    {
-              cerr << "BFGS update error1" << endl;
 	      (*testout) << "BFGS update error1" << endl;
 	      (*testout) << "l " << endl << l << endl
 			 << "d " << d << endl;
@@ -341,7 +340,6 @@ double BFGS (
 
 	  if (LDLtUpdate (l, d, -1 / a2, bs) != 0)
 	    {
-              cerr << "BFGS update error2" << endl;
 	      (*testout) << "BFGS update error2" << endl;
 	      (*testout) << "l " << endl << l << endl
 			 << "d " << d << endl;
